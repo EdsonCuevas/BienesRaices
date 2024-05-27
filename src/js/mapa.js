47dd53918bb1e754da5c25f8c65a12ee41097b02
Edson <efelix.cuevas@gmail.com>
2024-05-27 01:50:05 -0600

    add pin in map

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -2,11 +2,19 @@
     const lat = 19.1134727;
     const lng = -104.3426422;
     const mapa = L.map('mapa').setView([lat, lng], 14);
+    let marker;
 
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(mapa);
+
+    // Pin
+    marker = new L.marker([lat, lng], {
+        draggable: true,
+        autoPan: true
+    })
+        .addTo(mapa)
 
 
 })()
