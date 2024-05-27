2b4cc58a03ad48fc387a1ae78f1ecf43a671ae0e
EdsonCuevas <efelix@ucol.mx>
2024-05-27 10:15:35 -0600

    add save lat and lng

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -4,6 +4,9 @@
     const mapa = L.map('mapa').setView([lat, lng], 14);
     let marker;
 
+    // Utilizar Provider y Geocoder
+    const geocodeService = L.esri.Geocoding.geocodeService();
+
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -21,6 +24,17 @@
         marker = e.target;
         const posicion = marker.getLatLng();
         mapa.panTo(new L.LatLng(posicion.lat, posicion.lng))
+
+        //Obtener nombre de las calles al soltar pin
+        geocodeService.reverse().latlng(posicion, 14).run(function (error, res) {
+            marker.bindPopup(res.address.LongLabel)
+
+            // Llenar los campos
+            document.querySelector('.calle').textContent = res?.address?.Address ?? '';
+            document.querySelector('#calle').value = res?.address?.Address ?? '';
+            document.querySelector('#lat').value = res?.latlng?.lat ?? '';
+            document.querySelector('#lng').value = res?.latlng?.lng ?? '';
+        })
     })
 
 })()
